989c0334a81add47fd6091b14a1781028bcb04ef
Ahmad Avtorkhanov <example0612@mail.ru>
2023-04-14 17:37:57 +0300

    Tasks for chek - 22.04

--- Tasks.py
+++ Tasks.py
@@ -1,125 +1,189 @@
-# Задачи на 15.04.2023.
-
-# модуль ООП. Тема: классы и объекты.
+# ООП - 3. 22.04.23
 #
 # Задание 1
-# Реализуйте класс «Автомобиль». Необходимо хранить
-# в полях класса: название модели, год выпуска, производителя, объем двигателя, цвет машины, цену. Реализуйте
-# методы класса для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса.
+# Создайте класс Circle (окружность). Для данного
+# класса реализуйте ряд перегруженных операторов:
+# ■ Проверка на равенство радиусов двух окружностей
+# (операция = =);
+# ■ Сравнения длин двух окружностей (операции >, <,
+# <=,>=);
+# ■ Пропорциональное изменение размеров окружности,
+# путем изменения ее радиуса (операции + - += -=).
 
-
-# class Car:
-#     def __init__(self, model_name, year_of_manufacture, manufacturer, engine_size, car_color, price):
-#         self.model_name = model_name
-#         self.year_of_manufacture = year_of_manufacture
-#         self.manufacturer = manufacturer
-#         self.engine_size = engine_size
-#         self.car_color = car_color
-#         self.price = price
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius = radius
 #
-#     def set_info(self):
-#         self.model_name = input('Введите модель: ')
-#         self.year_of_manufacture = input('Введите год выпуска: ')
-#         self.manufacturer = input('Введите производителя: ')
-#         self.engine_size = input('Введите объем двигателя: ')
-#         self.car_color = input('Введите цвет машины: ')
-#         self.price = input('Введите цену: ')
+#     def __eq__(self, other):
+#         return self.radius == other.radius
 #
-#     def print_info(self):
-#         print(f'Модель: {self.model_name}\n'
-#               f'Год выпуска: {self.year_of_manufacture} г\n'
-#               f'Производитель: {self.manufacturer}\n'
-#               f'Объем двигателя: {self.engine_size} л\n'
-#               f'Цвет машины: {self.car_color}\n'
-#               f'Цена: {self.price} руб\n')
+#     def __lt__(self, other):
+#         return self.radius < other.radius
 #
+#     def __gt__(self, other):
+#         return self.radius > other.radius
 #
-# Car1 = Car('Лада-Гранта',
-#            '2015',
-#            'Россия',
-#            '1.6',
-#            'Белый',
-#            '300 000')
+#     def __le__(self, other):
+#         return self.radius <= other.radius
 #
-# Car1.print_info()
-# Car1.set_info()
-# Car1.print_info()
-
+#     def __ge__(self, other):
+#         return self.radius >= other.radius
+#
+#     def __add__(self, other):
+#         return Circle(self.radius + other.radius)
+#
+#     def __sub__(self, other):
+#         return Circle(self.radius - other.radius)
+#
+#     def __iadd__(self, other):
+#         self.radius += other
+#         return self
+#
+#     def __isub__(self, other):
+#         self.radius -= other
+#         return self
+#
+# c1 = Circle(5)
+# c2 = Circle(7)
+#
+# print(c1 == c2)
+# print(c1 < c2)
+# print(c1 > c2)
+# print(c1 <= c2)
+# print(c1 >= c2)
+#
+# c3 = c1 + c2
+# print(c3.radius)
 
 # Задание 2
-# Реализуйте класс «Книга». Необходимо хранить в
-# полях класса: название книги, год выпуска, издателя,
-# жанр, автора, цену. Реализуйте методы класса для ввода
-# данных, вывода данных, реализуйте доступ к отдельным
-# полям через методы класса.
+# Создайте класс Complex (комплексное число). Более
+# подробно ознакомиться с комплексными числами можно
+# по ссылке.
+# Создайте перегруженные операторы для реализации
+# арифметических операций для по работе с комплексными
+# числами (операции +, -, *, /).
 
-# class Book:
-#     def __init__(self, title_of_the_book, year_of_issue, publisher, genre, author, price):
-#         self.title_of_the_book = title_of_the_book
-#         self.year_of_issue = year_of_issue
-#         self.publisher = publisher
-#         self.genre = genre
-#         self.author = author
-#         self.price = price
-#     def set_info(self):
-#         self.title_of_the_book = input('Введите название книги: ')
-#         self.year_of_issue = input('Введите год выпуска: ')
-#         self.publisher = input('Введите издателя: ')
-#         self.genre = input('Введите Жанр: ')
-#         self.author = input('Введите Автора: ')
-#         self.price = input('Введите цену: ')
-#     def print_info(self):
-#         print(f'Название книги: {self.title_of_the_book}\n'
-#               f'Год выпуска: {self.year_of_issue}\n'
-#               f'Издательство: {self.publisher}\n'
-#               f'Жанр: {self.genre}\n'
-#               f'Автор: {self.author}\n'
-#               f'Цена: {self.price}\n')
+# class Complex:
+#     def __init__(self, real, imag):
+#         self.real = real
+#         self.imag = imag
 #
-# Book1 = Book('Сицилиец',
-#              '1969',
-#               'Random House',
-#               'Зарубежная классика',
-#               'Марио Пьюзо',
-#               '550')
-# Book1.print_info()
-# Book1.set_info()
-# Book1.print_info()
+#     def __add__(self, other):
+#         return Complex(self.real + other.real, self.imag + other.imag)
+#
+#     def __sub__(self, other):
+#         return Complex(self.real - other.real, self.imag - other.imag)
+#
+#     def __mul__(self, other):
+#         return Complex(self.real * other.real - self.imag * other.imag, self.real * other.imag + self.imag * other.real)
+#
+#     def __truediv__(self, other):
+#         d = other.real ** 2 + other.imag ** 2
+#         return Complex((self.real * other.real + self.imag * other.imag) / d, (self.imag * other.real - self.real * other.imag) / d)
+#
+#     def __str__(self):
+#         return f"{self.real} + {self.imag}i"
+#
+#
+# c1 = Complex(1, 2)
+# c2 = Complex(3, 4)
+#
+# c3 = c1 + c2
+# print(c3)
+#
+# c4 = c2 - c1
+# print(c4)
 
 # Задание 3
-# Реализуйте класс «Стадион». Необходимо хранить в
-# полях класса: название стадиона, дату открытия, страну,
-# город, вместимость. Реализуйте методы класса для ввода
-# данных, вывода данных, реализуйте доступ к отдельным
-# полям через методы класса.
+# Вам необходимо создать класс Airplane (самолет).
+# ■ Проверка на равенство типов самолетов (операция = =);
+# ■ Увеличение и уменьшение пассажиров в салоне самолета (операции + - += -=);
+# ■ Сравнение двух самолетов по максимально возможному количеству пассажиров на борту (операции >
+# < <= >=).
 
-# class Stadium:
-#     def __init__(self, stadium_name, opening_date, country, city, capacity):
-#         self.stadium_name = stadium_name
-#         self.opening_date = opening_date
-#         self.country = country
-#         self.city = city
-#         self.capacity = capacity
+# class Airplane:
+#     def __init__(self, airplane_type, max_passengers, current_passengers):
+#         self.airplane_type = airplane_type
+#         self.max_passengers = max_passengers
+#         self.current_passengers = current_passengers
 #
-#     def set_info(self):
-#         self.stadium_name = input('Введите Название стадиона: ')
-#         self.opening_date = input('Введите Дату открытия: ')
-#         self.country = input('Введите Страну: ')
-#         self.city = input('Введите Город: ')
-#         self.capacity = input('Введите вместимость: ')
+#     def __eq__(self, other):
+#         return self.airplane_type == other.airplane_type
 #
-#     def print_info(self):
-#         print(f'Название стадиона: {self.stadium_name}\n'
-#         f'Год открытия: {self.opening_date}\n'
-#         f'Страна: {self.country}\n'
-#         f'Город: {self.city}\n'
-#         f'Вместимость: {self.capacity}\n')
+#     def __add__(self, other):
+#         return Airplane(self.airplane_type, self.max_passengers + other, self.current_passengers + other)
 #
-# Stadium1 = Stadium('Краснодар',
-#                    '2016',
-#                    'Россия',
-#                    'Краснодар',
-#                    '35 179')
-# Stadium1.print_info()
-# Stadium1.set_info()
-# Stadium1.print_info()
+#     def __iadd__(self, other):
+#         self.max_passengers += other
+#         self.current_passengers += other
+#         return self
+#
+#     def __sub__(self, other):
+#         return Airplane(self.airplane_type, self.max_passengers - other, self.current_passengers - other)
+#
+#     def __isub__(self, other):
+#         self.max_passengers -= other
+#         self.current_passengers -= other
+#         return self
+#
+#     def __gt__(self, other):
+#         return self.max_passengers > other.max_passengers
+#
+#     def __lt__(self, other):
+#         return self.max_passengers < other.max_passengers
+#
+#     def __ge__(self, other):
+#         return self.max_passengers >= other.max_passengers
+#
+#     def __le__(self, other):
+#         return self.max_passengers <= other.max_passengers
+#
+# airplane1 = Airplane("Boeing 737", 200, 100)
+# airplane2 = Airplane("Boeing 747", 300, 200)
+#
+# print(airplane1 == airplane2)
+# print(airplane1 == Airplane("Boeing 737", 150, 75))
+#
+# airplane1 += 50
+# print(airplane1.max_passengers)
+# print(airplane1.current_passengers)
+
+
+# Задание 4
+#Создать класс Flat (квартира). Реализовать перегруженные операторы:
+# ■ Проверка на равенство площадей квартир (операция
+# ==);
+# ■ Проверка на неравенство площадей квартир (операция !=);
+# ■ Сравнение двух квартир по цене (операции > < <= >=).
+# class Flat:
+#     def __init__(self, area, price):
+#         self.area = area
+#         self.price = price
+#
+#     def __eq__(self, other):
+#         return self.area == other.area
+#
+#     def __ne__(self, other):
+#         return self.area != other.area
+#
+#     def __lt__(self, other):
+#         return self.price < other.price
+#
+#     def __le__(self, other):
+#         return self.price <= other.price
+#
+#     def __gt__(self, other):
+#         return self.price > other.price
+#
+#     def __ge__(self, other):
+#         return self.price >= other.price
+#
+# flat1 = Flat(50, 100000)
+# flat2 = Flat(60, 120000)
+# flat3 = Flat(50, 90000)
+#
+# print(flat1 == flat2)
+# print(flat1 == flat3)
+# print(flat1 != flat2)
+# print(flat1 > flat2)
+>>>>>>> c4cf159 (Tasks for chek - 22.04)
